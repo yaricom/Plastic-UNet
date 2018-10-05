0d7694a9f19a4fa7f2ead3ff96da3e2c9d2fbd51
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2018-10-05 17:43:19 +0300

    Implemented validation with no_grad mode

--- src/eval.py
+++ src/eval.py
@@ -2,6 +2,7 @@
 
 import torch
 from torch.autograd import Variable
+from torch.autograd import no_grad
 
 import numpy as np
 
@@ -21,26 +22,27 @@ def eval_net(net, X_val, y_val, device, criterion):
         (accuracy, loss) the tuple with validation accuracy against IoU metric as well as validation loss
     """
     net.eval()
-    hebb = net.initialZeroHebb()
+    with torch.no_grad():
+        hebb = net.initialZeroHebb()
 
-    val_loss = 0
-    total_acc = 0
-    for img, mask in zip(X_val, y_val):
-        t_img = torch.from_numpy(np.array([img.astype(np.float32)])).to(device)
-        mask_val = torch.from_numpy(mask.astype(np.float32)).to(device)
+        val_loss = 0
+        total_acc = 0
+        for i, d in enumerate(zip(X_val, y_val)):
+            t_img = torch.from_numpy(np.array([d[0].astype(np.float32)])).to(device)
+            mask_val = torch.from_numpy(d[1].astype(np.float32)).to(device)
 
-        # We do not learn plasticity within validation
-        mask_pred, _ = net(Variable(t_img, requires_grad=False), Variable(hebb, requires_grad=False))
+            # We do not learn plasticity within validation
+            mask_pred, _ = net(Variable(t_img, requires_grad=False), Variable(hebb, requires_grad=False))
 
-        y_pred_flat = mask_pred.view(-1)
-        y_target_flat = mask_val.view(-1)
+            y_pred_flat = mask_pred.view(-1)
+            y_target_flat = mask_val.view(-1)
 
-        # The validation loss
-        loss = criterion(y_pred_flat, y_target_flat)
-        val_loss += loss.item()
+            # The validation loss
+            loss = criterion(y_pred_flat, y_target_flat)
+            val_loss += loss.item()
 
-        # The validation accuracy
-        acc = iou_metric(y_pred_in=y_pred_flat.detach().numpy(), y_true_in=y_target_flat.detach().numpy(), print_table=True)
-        total_acc += acc
+            # The validation accuracy
+            acc = iou_metric(y_pred_in=y_pred_flat.detach().numpy(), y_true_in=y_target_flat.detach().numpy(), print_table=True)
+            total_acc += acc
 
-    return (total_acc / len(X_val), val_loss / len(X_val))
+    return (total_acc / (i + 1), val_loss / (i + 1))
