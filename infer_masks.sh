c5407d42558274d28c8170486128529312039fde
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2018-10-08 15:49:14 +0300

    Added submission file generation

--- infer_masks.sh
+++ infer_masks.sh
@@ -11,5 +11,5 @@ help () {
 }
 
 /usr/bin/env python3 src/infer.py --model ./out/train_data_net.pth \
-                                  --data ./data1/dataset.hdf5 --out ./out \
+                                  --data ./data1 --out ./out \
                                   --visualize True --save True
--- src/infer.py
+++ src/infer.py
@@ -9,30 +9,28 @@ from torch.autograd import no_grad
 
 import h5py
 import numpy as np
+import pandas as pd
 from skimage.transform import resize
 from skimage.io import imsave
 
 from unet import UNetp
-from utils import hwc_to_chw
-from utils import chw_to_hwc
-from utils import plot_image_mask
 
-# Set some parameters
-out_im_width = 101
-out_im_height = 101
-im_chan = 3
-debug = True
+from utils import hwc_to_chw
+from utils import plot_image_mask
+from utils import plot_test_check
+from utils import load_image
+
+from utils import encode
 
 def inference(net,
               img_data,
-              device,
-              mask_threshold=0.5):
+              device):
     """
     Do mask inference for provided image.
     Arguments:
-        net: The trined model
-        img_data: The input image data for inference
-        device: The Torch device to be used
+        net:            The trained network
+        img_data:       The input image data for inference
+        device:         The Torch device to be used
         mask_threshold: The minimum probability value to consider a mask pixel white
     Return:
         the predicted mask image
@@ -45,63 +43,115 @@ def inference(net,
         y_pred, _ = net(Variable(t_img, requires_grad=False), Variable(hebb, requires_grad=False))
 
     mask = y_pred.squeeze().cpu().numpy()
-    return mask > mask_threshold
+    return mask
 
-def main(args):
+def predict(data_dir,
+            out_dir,
+            model,
+            subm_file="submission.csv",
+            gpu=True,
+            mask_threshold=0.5,
+            visualize=False,
+            save_masks=False,
+            img_width=128,
+            img_height = 128,
+            out_img_width=101,
+            out_img_height=101,
+            img_chan=3,
+            debug=False):
     """
-    Iterate over all test images and do masks inference
+    Iterate over all test images and do masks prediction
+    Arguments:
+        data_dir:       The directory to look for test data
+        out_dir:        The directory to save results
+        model:          The trained network model and state dictionary
+        subm_file:      The file name of submission file
+        gpu:            The flag to indicate whether to use GPU for inference
+        mask_threshold: The minimum probability value to consider a mask pixel white
+        visualize:      The flag to indicate whether to visualize the images as they are processed
+        save_masks:     The flag to indicate whether to save the output masks
+        img_width:      The width of the resized image
+        img_height:     The height of the resized image
+        out_img_width:  The width of the original image
+        out_img_height: The height of the original image
+        img_chan:       The number of channels in input plot_image
+        debug:          The flag to indicate whether to show debug information
     """
     # Check if output directory exists
-    if not os.path.isdir(args.out_dir):
-        os.mkdir(args.out_dir)
+    if not os.path.isdir(out_dir):
+        os.mkdir(out_dir)
 
     # Create torch device for tensor operations
-    args.device = None
-    if args.gpu and torch.cuda.is_available():
-        args.device = torch.device('cuda')
+    device = None
+    if gpu and torch.cuda.is_available():
+        device = torch.device('cuda')
     else:
-        args.device = torch.device('cpu')
+        device = torch.device('cpu')
 
-    net = UNetp(n_channels=im_chan, n_classes=1, device=args.device)
-    print("Loading model %s" % (args.model))
+    net = UNetp(n_channels=img_chan, n_classes=1, device=device)
 
-    net.load_state_dict(torch.load(args.model))
-    net.to(args.device)
+    print("Loading model %s" % (model))
+    net.load_state_dict(torch.load(model))
+    net.to(device)
 
     # Get test images
-    print('Getting test imagesfrom dataset... ')
-    sys.stdout.flush()
-    with h5py.File(args.data_file, 'r') as f:
-        X = f['test/images'][()]
+    print('Getting and resizing test images... ')
+    test_ids = next(os.walk(data_dir + "/test/images"))[2]
+    X_test = np.zeros((len(test_ids), img_height, img_width, img_chan), dtype=np.float64)
+    for n, id_ in enumerate(test_ids):
+        x = load_image(data_dir + '/test/images/' + id_, (img_height, img_width, img_chan))
+        X_test[n] = x
+
     print('Done!')
 
+    #
+    # Check if test data looks all right
+    #
+    if debug:
+        plot_test_check(X_test)
+
     # transpose HWC to CHW image data format accepted by Torch
-    X_test = list(map(hwc_to_chw, X))
+    X_test = list(map(hwc_to_chw, X_test))
 
     # iterate over test images and do inference
     preds_downsampled = []
     for i, img_data in enumerate(X_test):
         mask = inference(net=net,
                          img_data=img_data,
-                         device=args.device,
-                         mask_threshold=args.mask_threshold)
+                         device=device)
 
-        mask = resize(mask, (out_im_height, out_im_width), mode='constant')
+        mask = resize(mask, (out_img_height, out_img_width), mode='constant', preserve_range=True)
         preds_downsampled.append(mask)
 
-        if args.visualize:
-            image = resize(img_data, (im_chan, out_im_height, out_im_width), mode='constant')
-            image = np.transpose(image, (1, 2, 0))
-            plot_image_mask(image, mask)
+        print(mask)
 
-        if args.save:
+
+        if visualize:
+            image = resize(img_data, (img_chan, out_img_height, out_img_width), mode='constant')
+            image = np.transpose(image, (1, 2, 0))
+            mask_t = (mask > mask_threshold).astype(np.uint8)
+            plot_image_mask(image, mask_t)
+
+        if save_masks:
             out_path = args.out_dir + "/masks/"
             if not os.path.isdir(out_path):
                 os.mkdir(out_path)
 
-            imsave(out_path + str(i) + ".png", mask)
+            mask_t = (mask > mask_threshold).astype(np.uint8)
+            tmp = np.squeeze(mask_t).astype(np.float32)
+            imsave(out_path + test_ids[i], np.dstack((tmp,tmp,tmp)))
 
+    # Sanity check
+    print(preds_downsampled[0].shape)
 
+    # prepare submission data in CSV file as RLE encoded
+    pred_dict = {fn[:-4]:encode(np.round(preds_downsampled[i])) for i,fn in enumerate(test_ids)}
+
+    # save to submission file
+    sub = pd.DataFrame.from_dict(pred_dict,orient='index')
+    sub.index.names = ['id']
+    sub.columns = ['rle_mask']
+    sub.to_csv(out_dir + "/" + subm_file)
 
 def get_args():
     """
@@ -111,8 +161,8 @@ def get_args():
     parser.add_option('--model', '-m', default='MODEL.pth',
                         help="Specify the file in which is stored the model"
                              " (default : 'MODEL.pth')")
-    parser.add_option('-i', '--data', dest='data_file', type='string',
-                      help='the path to the dataset file with input data')
+    parser.add_option('-i', '--data', dest='data_dir', type='string',
+                      help='the directory with input test data')
 
     parser.add_option('--out', '-o', dest='out_dir', default='./out',
                         help='directory for ouput images')
@@ -135,4 +185,11 @@ def get_args():
 
 if __name__ == "__main__":
     args = get_args()
-    main(args)
+    predict(model=args.model,
+            data_dir=args.data_dir,
+            out_dir=args.out_dir,
+            gpu=args.gpu,
+            mask_threshold=args.mask_threshold,
+            visualize=args.visualize,
+            save_masks=args.save,
+            debug=True)
