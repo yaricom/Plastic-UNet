f941e816ad072efec1b555d5ea6920a3acf81816
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2018-10-25 18:58:30 +0300

    Minor fixes

--- src/unet/unet_p.py
+++ src/unet/unet_p.py
@@ -6,17 +6,16 @@ from torch.autograd import Variable
 
 
 class UNetp(nn.Module):
-    def __init__(self, n_channels, n_classes, device, neurons=16, alfa_type='free', rule='hebb', nbf=128, batch_norm=False, bilinear_upsample=False):
+    def __init__(self, n_channels, n_classes, device, alfa_type='free', rule='hebb', nbf=128, batch_norm=False, bilinear_upsample=False):
         """
         Creates new U-Net network with plastic learning rule implemented
         Arguments:
             n_channels: The number of input n_channels
-            n_classes:  The number of ouput classes to be learned
-            device:     The torch device to execute tensor operations
-            neurons:    The # of neurons for the first leayer
-            alfa_type:  The plasticity coefficient ['free', 'yoked'] (if the latter, alpha is a single scalar learned parameter, shared across all connection)
-            rule:       The name of plasticity rule to apply ['hebb', 'oja'] (The Oja rule can maintain stable weight values indefinitely in the absence of stimulation, thus allowing stable long-term memories, while still preventing runaway divergences)
-            nbf:        The number of features in plasticity rule vector (width * height)
+            n_classes: The number of ouput classes to be learned
+            device: The torch device to execute tensor operations
+            alfa_type: The plasticity coefficient ['free', 'yoked'] (if the latter, alpha is a single scalar learned parameter, shared across all connection)
+            rule: The name of plasticity rule to apply ['hebb', 'oja'] (The Oja rule can maintain stable weight values indefinitely in the absence of stimulation, thus allowing stable long-term memories, while still preventing runaway divergences)
+            nbf: The number of features in plasticity rule vector (width * height)
         """
         super(UNetp, self).__init__()
 
@@ -34,17 +33,17 @@ class UNetp(nn.Module):
 
 
         # The DOWN network structure
-        self.inc = inconv(n_channels, neurons, batch_norm=batch_norm)
-        self.down1 = down(neurons, neurons * 2, batch_norm=batch_norm)
-        self.down2 = down(neurons * 2, neurons * 4, batch_norm=batch_norm)
-        self.down3 = down(neurons * 4, neurons * 8, batch_norm=batch_norm)
-        self.down4 = down(neurons * 8, neurons * 8, batch_norm=batch_norm)
+        self.inc = inconv(n_channels, 8, batch_norm=batch_norm)
+        self.down1 = down(8, 16, batch_norm=batch_norm)
+        self.down2 = down(16, 32, batch_norm=batch_norm)
+        self.down3 = down(32, 64, batch_norm=batch_norm)
+        self.down4 = down(64, 64, batch_norm=batch_norm)
         # The UP network structure
-        self.up1 = up(neurons * 16, neurons * 4, batch_norm=batch_norm, bilinear=bilinear_upsample)
-        self.up2 = up(neurons * 8, neurons * 2, batch_norm=batch_norm, bilinear=bilinear_upsample)
-        self.up3 = up(neurons * 4, neurons, batch_norm=batch_norm, bilinear=bilinear_upsample)
-        self.up4 = up(neurons * 2, neurons, batch_norm=batch_norm, bilinear=bilinear_upsample)
-        self.outc = outconv(neurons, n_classes)
+        self.up1 = up(128, 32, batch_norm=batch_norm, bilinear=bilinear_upsample)
+        self.up2 = up(64, 16, batch_norm=batch_norm, bilinear=bilinear_upsample)
+        self.up3 = up(32, 8, batch_norm=batch_norm, bilinear=bilinear_upsample)
+        self.up4 = up(16, 8, batch_norm=batch_norm, bilinear=bilinear_upsample)
+        self.outc = outconv(8, n_classes)
 
          # Move network parameters to the specified device
         self.to(device)
@@ -68,7 +67,6 @@ class UNetp(nn.Module):
         x = self.outc(x)
 
         # The Plasticity rule implementation
-        print(x.shape)
         activin = x.view(self.nbf, self.nbf) # The batch size assumed to be 1
 
         if self.alfa_type == 'free':
@@ -95,50 +93,88 @@ class UNetp(nn.Module):
         """
         return torch.zeros(self.nbf, self.nbf, dtype=torch.float, device=self.torch_dev)
 
-class conv_module(nn.Module):
+class double_conv(nn.Module):
     """
-    The simple convolution module with optional batch normalization and activation
+    Creates two subsequent unpadded convolution layers with 3x3 kernel size
+    followed by batch normalization (optional) and ReLU
     """
-    def __init__(self, in_ch, out_ch, size, strides=(1,1), padding=1, activation=True, batch_norm=False):
-        super(conv_module, self).__init__()
-        if batch_norm == True:
+    def __init__(self, in_ch, out_ch, batch_norm):
+        super(double_conv, self).__init__()
+        if batch_norm:
             self.conv = nn.Sequential(
-                nn.Conv2d(in_ch, out_ch, kernel_size=size, strides=strides, padding=padding),
-                nn.BatchNorm2d(out_ch)
+                nn.Conv2d(in_ch, out_ch, 3, padding=1),
+                nn.BatchNorm2d(out_ch),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(out_ch, out_ch, 3, padding=1),
+                nn.BatchNorm2d(out_ch),
+                nn.ReLU(inplace=True)
             )
         else:
             self.conv = nn.Sequential(
-                nn.Conv2d(in_ch, out_ch, kernel_size=size, strides=strides, padding=padding)
+                nn.Conv2d(in_ch, out_ch, 3, padding=1),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(out_ch, out_ch, 3, padding=1),
+                nn.ReLU(inplace=True)
             )
-
-        if activation == True:
-            self.conv.add_module(nn.ReLU(inplace=True))
 
     def forward(self, x):
         x = self.conv(x)
         return x
 
-class residual_block(nn.Module):
 
-    def __init__(self, in_ch, out_ch, batch_norm=False):
-        super(residual_block, self).__init__()
-        if batch_norm == True:
-            self.conv = nn.Sequential(
-                nn.ReLU(inplace=True),
-                nn.BatchNorm2d(out_ch),
-                conv_module(in_ch, out_ch, kernel_size=3)
-                conv_module(in_ch, out_ch, kernel_size=3, activation=False)
-            )
+class inconv(nn.Module):
+    def __init__(self, in_ch, out_ch, batch_norm=True):
+        super(inconv, self).__init__()
+        self.conv = double_conv(in_ch, out_ch, batch_norm)
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+
+class down(nn.Module):
+    def __init__(self, in_ch, out_ch, batch_norm=True):
+        super(down, self).__init__()
+        self.mpconv = nn.Sequential(
+            nn.MaxPool2d(2),
+            double_conv(in_ch, out_ch, batch_norm)
+        )
+
+    def forward(self, x):
+        x = self.mpconv(x)
+        return x
+
+
+class up(nn.Module):
+    def __init__(self, in_ch, out_ch, bilinear=True, batch_norm=True):
+        super(up, self).__init__()
+
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
         else:
-            self.conv = nn.Sequential(
-                nn.ReLU(inplace=True),
-                conv_module(in_ch, out_ch, kernel_size=3)
-                conv_module(in_ch, out_ch, kernel_size=3, activation=False)
-            )
+            self.up = nn.ConvTranspose2d(in_ch//2, in_ch//2, 2, stride=2)
 
-    def forward(self, input):
-        x = self.conv(input)
-        return x.add(input)
+        self.conv = double_conv(in_ch, out_ch, batch_norm)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        diffX = x1.size()[2] - x2.size()[2]
+        diffY = x1.size()[3] - x2.size()[3]
+        x2 = F.pad(x2, (diffX // 2, int(diffX / 2),
+                        diffY // 2, int(diffY / 2)))
+        x = torch.cat([x2, x1], dim=1)
+        x = self.conv(x)
+        return x
+
+
+class outconv(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(outconv, self).__init__()
+        self.conv = nn.Conv2d(in_ch, out_ch, 1)
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
 
 class double_conv(nn.Module):
     """
