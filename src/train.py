a01308b0c7a14eb8e33ffbad8c29fa54c8821aa9
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2018-10-26 18:07:59 +0300

    Data set loading extracted

--- src/train.py
+++ src/train.py
@@ -9,7 +9,6 @@ import warnings
 from optparse import OptionParser
 
 import h5py
-from sklearn.model_selection import train_test_split
 import numpy as np
 import pandas as pd
 
@@ -21,11 +20,8 @@ from torch import optim
 from unet import UNetp
 from unet import UNetpRes
 
-from utils import plot_train_check
 from utils import load_image
-
-from utils import plot_coverage
-from utils import plot_depth
+from utils import load_train_dataset
 
 from eval import eval_net
 
@@ -118,8 +114,9 @@ def train(net, X_train, X_val, y_train, y_val, params):
 
         epoch_time = time.time() - epoch_start_time
         next_epoch_finish_time = epoch_time + time.time()
-        # check if need to force stop training due to time limits
-        terminate_training = (params['stop_time'] > 0 and next_epoch_finish_time >= params['stop_time'])
+
+        # check if need to force stop training due to time limits or due to epochs limits
+        terminate_training = (params['stop_time'] > 0 and next_epoch_finish_time >= params['stop_time']) or (epoch + 1) == params['epochs']
 
         if params['debug']:
             print('Epoch finished! Loss: %f, time spent: %d, terminate due to time limits: %s' %
@@ -128,7 +125,7 @@ def train(net, X_train, X_val, y_train, y_val, params):
         #
         # Perform validation
         #
-        if (epoch + 1) % params['val_every'] == 0:
+        if (epoch + 1) % params['val_every'] == 0 or terminate_training:
             val_acc, val_loss = eval_net(net,
                                          X_val=X_val,
                                          y_val=y_val,
@@ -150,7 +147,7 @@ def train(net, X_train, X_val, y_train, y_val, params):
         #
         # Save checkpoint if appropriate
         #
-        if (epoch + 1) % params['save_every'] == 0 or (epoch + 1) == params['epochs'] or terminate_training:
+        if (epoch + 1) % params['save_every'] == 0 or terminate_training:
             if params['debug']:
                 print("Saving checkpoint files for epoch:", epoch)
 
@@ -209,30 +206,6 @@ def train(net, X_train, X_val, y_train, y_val, params):
             print("Stop time limit: %d, estimated time of next epoch end: %d" %
                     (params['stop_time'], next_epoch_finish_time))
             break
-"""
-def find_best_threshold(net,
-                        data_dir,
-                        img_width=128,
-                        img_height = 128,
-                        img_chan=3,
-                        gpu=True,
-                        visualize=False,
-                        save_results=False,
-                        debug=False):
-
-    Finds best threshold value for IoU metric against validation data.
-    Arguments:
-        net: The trained network to use for inference
-        data_dir: The directory with data samples
-        img_width:      The width of the resized image
-        img_height:     The height of the resized image
-        visualize:      The flag to indicate whether to plot results
-        save_masks:     The flag to indicate whether to save the results
-        img_chan:       The number of channels in input plot_image
-        debug:          The flag to indicate whether to show debug information
-    """
-
-
 
 def start_train(x_train, x_valid, y_train, y_valid,
                 out_dir,
@@ -316,6 +289,7 @@ def start_train(x_train, x_valid, y_train, y_valid,
 
     if load:
         net.load_state_dict(torch.load(model))
+        net.to(device)
         print('Model loaded from %s' % (model))
 
     # do network training
@@ -335,61 +309,6 @@ def start_train(x_train, x_valid, y_train, y_valid,
             os._exit(0)
 
     return net
-
-def cov_to_class(val):
-    for i in range(0, 11):
-        if val * 10 <= i :
-            return i
-
-def load_train_dataset( data_dir,
-                        img_width,
-                        img_height,
-                        img_chan,
-                        val_ratio=0.2,
-                        debug=False):
-    """
-    Loads train data set from provided directory and split it onto train and validation.
-    Arguments:
-        data_dir:   The data directory to load training data
-        img_width:  The target image width
-        img_height: The target image height
-        img_chan:   The target image channels
-        val_ratio:  The ratio to split train/test data
-    Returns:
-        The preprocessed dataset with data samples and target labels for training
-        and validation.
-    """
-    # Create data DataFrame
-    train_df = pd.read_csv(data_dir + '/train.csv', index_col='id', usecols=[0])
-    depths_df = pd.read_csv(data_dir + '/depths.csv', index_col='id')
-    train_df = train_df.join(depths_df)
-    test_df = depths_df[~depths_df.index.isin(train_df.index)]
-
-    # Load image data
-    train_df["images"] = [np.array(load_image('{}/train/images/{}.png'.format(data_dir, idx), (img_height, img_width))) for idx in train_df.index]
-    train_df["masks"] = [np.array(load_image('{}/train/masks/{}.png'.format(data_dir, idx), (img_height, img_width))) / 65535 for idx in train_df.index]
-
-    # Calculate and add salt coverage data
-    train_df["coverage"] = train_df.masks.map(np.sum) / (img_height * img_width)
-    train_df["coverage_class"] = train_df.coverage.map(cov_to_class)
-
-    # Plots salt coverage and depth distributions
-    if debug:
-        print(train_df.masks[10])
-        plot_coverage(train_df)
-        plot_depth(train_df, test_df)
-
-    # Create train/validation split stratified by salt coverage
-    ids_train, ids_valid, x_train, x_valid, y_train, y_valid, cov_train, cov_test, depth_train, depth_test = train_test_split(
-        train_df.index.values,
-        np.array(train_df.images.tolist()).reshape(-1, img_chan, img_height, img_width),
-        np.array(train_df.masks.tolist()).reshape(-1, 1, img_height, img_width),
-        train_df.coverage.values,
-        train_df.z.values,
-        test_size=val_ratio, stratify=train_df.coverage_class, random_state=42)
-
-
-    return x_train, x_valid, y_train, y_valid
 
 def parse_args():
     """
---
+++ src/utils/data_set.py
@@ -0,0 +1,64 @@
+# The script to load data set and do split it accordingly
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+from utils import load_image
+
+from utils import plot_coverage
+from utils import plot_depth
+
+def cov_to_class(val):
+    for i in range(0, 11):
+        if val * 10 <= i :
+            return i
+
+def load_train_dataset( data_dir,
+                        img_width,
+                        img_height,
+                        img_chan,
+                        val_ratio=0.2,
+                        debug=False):
+    """
+    Loads train data set from provided directory and split it onto train and validation.
+    Arguments:
+        data_dir:   The data directory to load training data
+        img_width:  The target image width
+        img_height: The target image height
+        img_chan:   The target image channels
+        val_ratio:  The ratio to split train/test data
+    Returns:
+        The preprocessed dataset with data samples and target labels for training
+        and validation.
+    """
+    # Create data DataFrame
+    train_df = pd.read_csv(data_dir + '/train.csv', index_col='id', usecols=[0])
+    depths_df = pd.read_csv(data_dir + '/depths.csv', index_col='id')
+    train_df = train_df.join(depths_df)
+    test_df = depths_df[~depths_df.index.isin(train_df.index)]
+
+    # Load image data
+    train_df["images"] = [np.array(load_image('{}/train/images/{}.png'.format(data_dir, idx), (img_height, img_width))) for idx in train_df.index]
+    train_df["masks"] = [np.array(load_image('{}/train/masks/{}.png'.format(data_dir, idx), (img_height, img_width))) / 65535 for idx in train_df.index]
+
+    # Calculate and add salt coverage data
+    train_df["coverage"] = train_df.masks.map(np.sum) / (img_height * img_width)
+    train_df["coverage_class"] = train_df.coverage.map(cov_to_class)
+
+    # Plots salt coverage and depth distributions
+    if debug:
+        print(train_df.masks[10])
+        plot_coverage(train_df)
+        plot_depth(train_df, test_df)
+
+    # Create train/validation split stratified by salt coverage
+    ids_train, ids_valid, x_train, x_valid, y_train, y_valid, cov_train, cov_test, depth_train, depth_test = train_test_split(
+        train_df.index.values,
+        np.array(train_df.images.tolist()).reshape(-1, img_chan, img_height, img_width),
+        np.array(train_df.masks.tolist()).reshape(-1, 1, img_height, img_width),
+        train_df.coverage.values,
+        train_df.z.values,
+        test_size=val_ratio, stratify=train_df.coverage_class, random_state=42)
+
+
+    return x_train, x_valid, y_train, y_valid
